26040a820d8327d70b129ebc4203a79d2a12b106
nakagaitoyu <s201044@matsu.shimane-u.ac.jp>
2023-01-26 13:26:14 +0900

    カリキュラム08-6.最終版

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,6 +5,7 @@
         <title>Blog</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        
     </head>
     <body class="antialiased">
         <h1>Blog Name</h1>
@@ -12,13 +13,27 @@
         <div class='posts'>
             @foreach($posts as $post)
               <div class='post'>
-                 <h2 class='title'>
-                     <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                 </h2>
+                <a href="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title}}</h2></a>
                  <p class='body'>{{ $post->body }}</p>
+                 <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                   @csrf
+                   @method('DELETE')
+                   <button type="button" onclick="deletePost({{ $post->id}})">delete</button>   
+                 </form>
+                
               </div>
             @endforeach
         </div>
         <div>{{$posts->links()}}</div>
+        <script>
+            function deletePost(id){
+                'use strict'
+                
+                if(confirm('削除すると復元できません。\本当に削除しますか？'))
+                {
+                 document.getElementById(`form_${id}`).submit(); 
+                }
+             }
+        </script>
     </body>
 </html>
